2f8227679addd224be39119c4df7dd970680253e
Michael Makarov <info1117@gmail.com>
2014-01-03 10:39:31 -0800

    Adds design

--- Gemfile
+++ Gemfile
@@ -13,6 +13,7 @@ group :production do
   gem 'pg'
 end
 
+gem 'bourbon'
 gem 'httparty'
 gem 'haml-rails'
 gem 'angularjs-rails'
--- config/initializers/omni.rb
+++ config/initializers/omni.rb
@@ -1,6 +1,6 @@
-Rails.application.config.middleware.use OmniAuth::Builder do
-  KEY = "226574923986-piarv0928u5qe2fij1qm5g03prklivsf.apps.googleusercontent.com"
-  SECRET = "C6GZVErRS8dsmwx2yE9mpKmp"
+KEY = Rails.env.production? ? ENV["GOOGLE_CLIENT"] : "226574923986-piarv0928u5qe2fij1qm5g03prklivsf.apps.googleusercontent.com"
+SECRET = Rails.env.production? ? ENV["GOOGLE_SECRET"] : "C6GZVErRS8dsmwx2yE9mpKmp"
 
-  provider :google_oauth2, KEY, SECRET #ENV["GOOGLE_KEY"], ENV["GOOGLE_SECRET"]
+Rails.application.config.middleware.use OmniAuth::Builder do
+  provider :google_oauth2, KEY, SECRET
 end
